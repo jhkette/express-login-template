58b9753d83eac9dc26d9d20a2aea6267b6effbd0
khenryjoe <joseph.ketterer@gmail.com>
2019-02-07 19:39:10 +0000

    saving user to db after registering

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 //login
 router.get('/login', (req, res) => res.render('login'));
 //register
@@ -14,38 +16,70 @@ router.post('/register', (req, res) => {
         password2
     } = req.body;
     let errors = [];
-    if(!name || !email || !password || !password2){
-        errors.push({msg: 'please fill in all fields'});
+    if (!name || !email || !password || !password2) {
+        errors.push({
+            msg: 'please fill in all fields'
+        });
     }
 
-    if(password !== password2){
-        errors.push({msg: 'passwords do not match'})
+    if (password !== password2) {
+        errors.push({
+            msg: 'passwords do not match'
+        })
 
     }
-    if(password.length < 6){
-        errors.push({msg: 'Password should be at least 6 charecters'})
+    if (password.length < 6) {
+        errors.push({
+            msg: 'Password should be at least 6 charecters'
+        })
     }
-    if(name.length < 3){
-        errors.push({msg: 'Name should be at least 3 charecters'})
+    if (name.length < 3) {
+        errors.push({
+            msg: 'Name should be at least 3 charecters'
+        })
     }
-
-
-    if(errors.length > 0){
+    if (errors.length > 0) {
         console.log('there are errors')
-        res.render('register',{
-
+        res.render('register', {
             errors,
             name,
             email,
             password,
             password2
-
         });
 
 
-    }
-    else{
-        res.send('pass');
+    } else {
+        User.findOne({email: email})
+        .then(user =>{
+            if(user){
+                // user exists
+                errors.push({msg: 'email is already in use'})
+                res.render('register', {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2
+                });
+            } else{
+                const newUser = new User ({
+                    name,
+                    email,
+                    password
+                });
+                bcrypt.genSalt(10,(err, salt)=>
+                  bcrypt.hash(newUser.password, salt, (err, hash) =>{
+                      newUser.password = hash;
+                      newUser.save()
+                          .then(user =>{
+                              console.log(newUser);
+                              res.redirect('users/login');
+                          })
+                           .catch(err => console.log(err));
+                  }))
+            }
+        })
     }
 
 })
